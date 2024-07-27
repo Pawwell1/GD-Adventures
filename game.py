04264bf2afd086c2a7ae68fa36e61137160d360d
Pawwell <162881986+Pawwell1@users.noreply.github.com>
2024-07-27 11:54:11 +0300

    Added Place Holder Images

--- game.py
+++ game.py
@@ -1,21 +1,27 @@
 import sys
 import pygame
+import random
 
 class Game:
     def __init__(self):
         pygame.init()
 
         pygame.display.set_caption('GD Adventures')
-        self.screen = pygame.display.set_mode((640, 480))
+        self.screen = pygame.display.set_mode((1920, 1080))
         self.Icon = pygame.image.load('Resources/Profile Circle.png')
         pygame.display.set_icon(self.Icon)
         self.img = pygame.image.load('Resources/gk-icon.png')
+        self.img2 = pygame.image.load('Resources/BE_Export_File-uhd.png')
 
         self.clock = pygame.time.Clock()
+
+        self.img_pos = [160, 260]
+        self.img2_pos = [70, 69]
         
     def run(self):
         while True:
-            self.screen.blit(self.img, (100, 100))
+            self.screen.blit(self.img, self.img_pos)
+            self.screen.blit(self.img2, self.img2_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
