fea353364c778eecc210160403996713fe8f1d27
Pawwell <162881986+Pawwell1@users.noreply.github.com>
2024-07-28 15:13:05 +0300

    Added Input For Movement (can only go up and down at the moment)

--- game.py
+++ game.py
@@ -10,24 +10,34 @@ class Game:
         self.screen = pygame.display.set_mode((1920, 1080))
         self.Icon = pygame.image.load('Resources/Profile Circle.png')
         pygame.display.set_icon(self.Icon)
-        self.img = pygame.image.load('Resources/gk-icon.png')
-        self.img2 = pygame.image.load('Resources/BE_Export_File-uhd.png')
+        self.img = pygame.image.load('Resources/Profile Circle.png')
 
         self.clock = pygame.time.Clock()
 
         self.img_pos = [160, 260]
-        self.img2_pos = [70, 69]
+        self.movement = [False, False]
         
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248))
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
             self.screen.blit(self.img, self.img_pos)
-            self.screen.blit(self.img2, self.img2_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+
             pygame.display.update()
             self.clock.tick(60)
 
